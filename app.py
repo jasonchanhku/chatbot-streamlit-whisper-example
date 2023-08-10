1d23fa504f770b4691ecd0292e468cb943bc073c
Jason Chan <jasonchanhku@gmail.com>
2023-08-11 02:53:35 +0800

    Update app.py
    added

--- app.py
+++ app.py
@@ -40,15 +40,16 @@ with input_container:
         # If it's coming from the audio recorder transcribe the message with whisper.cpp
         if len(audio)>0:
             prompt = inference(audio)
-    
-        # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-    
-        response = f"Echo: {prompt}"
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        with response_container:
+            # Display user message in chat message container
+            st.chat_message("user").markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        
+            response = f"Echo: {prompt}"
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
